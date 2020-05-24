89a37cdf08b637a44a666db1983a18953fea99aa
Hamza Carew <hamzacarew@gmail.com>
2020-05-24 14:57:19 +0100

    Implemented limit for numb of threada and further input validation

--- main.go
+++ main.go
@@ -2,37 +2,56 @@ package main
 
 import (
 	"os"
-	 // "github.com/sparrc/go-ping"
-	"log"
+	"github.com/sparrc/go-ping"
 	"net"
 	"fmt"
+	"strconv"
+)
+
+const (
+	threadLimit = 5000
 )
 
 func main() {
-	parseArgs()	
-
+	target, threads := parseArgs()	
+	fmt.Println(target, threads)
 
 }
 
-func parseArgs() (target, numbOfThreads string) {
+func parseArgs() (target string, numbOfThreads int) {
 	if len(os.Args) != 3 { 
 		fmt.Println("Error: Ensure you pass two arguments. Host/IP and the number of threads to spawn.")
 		os.Exit(1)
 	}
 
 	target = os.Args[1]
-	numbOfThreads = os.Args[2]
+	numbOfThreads, _ = strconv.Atoi(os.Args[2])
 
 	if ( isHostValid(target) ) == false {
 		fmt.Println(" Error: The host you entered is invalid.")
 		os.Exit(1)
 	}
+
+	if ( areNumberOfThreadsValid(numbOfThreads) ) == false {
+		fmt.Println("Error: The number of threads you enter exceeds the limit; ", threadLimit)
+		os.Exit(1)
+	}
+
 	return target, numbOfThreads 
 }
 
 func isHostValid(host string) bool {
-	result, err := net.LookupHost(host)
+	_ , err := net.LookupHost(host)
 	if err != nil { return false }
 
+	return true
+}
+
+func areNumberOfThreadsValid(threads int) bool {
+	threads = int(threads)
+
+	if threads > threadLimit {
+		return false
+	}
 	return true
 }
